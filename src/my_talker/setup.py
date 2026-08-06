ae2a5ac7a31276046cad6d987a6d65397e70c56a
sylvia09 <92027572+sylvia09@users.noreply.github.com>
2026-08-06 15:35:35 +0530

    Update src/my_talker/setup.py

---
+++ src/my_talker/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+package_name = 'my_talker'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
