39ece43ecc854a6ab14ef1ead759a815e65699bc
sylvia09 <92027572+sylvia09@users.noreply.github.com>
2026-08-06 16:03:58 +0530

    Update src/my_talker/setup.py

--- src/my_talker/setup.py
+++ src/my_talker/setup.py
@@ -7,7 +7,10 @@ setup(
     version='0.0.1',
     packages=[package_name],
     install_requires=['setuptools'],
-    entry_points={
-        'console_scripts': [],
+        entry_points={
+        'console_scripts': [
+            'my_node = my_talker.my_node:main',     # talker — already there from pkg create
+            'listener = my_talker.listener:main',   # ← add this line
+        ],
     },
 )
